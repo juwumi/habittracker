af62b4bce6cc36793370ad639d45d555b47e377f
ialek <taisya-alekseenko@mail.ru>
2026-03-05 19:02:02 +0300

    обновление класса Habit

--- include/habit.h
+++ include/habit.h
@@ -1,28 +1,38 @@
-#indef HABIT_H
-#endif HABIT_H
+#ifndef HABIT_H
+#define HABIT_H
 
 #include <string>
 #include<vector>
+#include <optional>
+#include <stdexcept>
+
+class HabitException: public std::runtime_error {
+public:
+    explicit HabitException(const std::string& message): std::runtime_error(message){};
+
+};
 
 class Habit{
 protected:
     std::string name;
-    int streak;
+    int streak=0;
+    int targetDaysPerWeek=7;
     std::vector<bool> history;
 public:
-    explicit Habit(std::string name);
-    virtual ~Habit();
-    virtual std::string getType() const = 0;
+    explicit Habit(const std::string& name, int targetDaysPerWeek=7);
+    virtual ~Habit()=default;
 
-    void markCompleted();
+    virtual std::string getType() const = 0;
+    virtual std::string toString() const=0;
+
+    void markCompleted(bool done);
     void updateStreak();
     double completionRate() const;
+    std::optional <bool> getLastEntry() const;
 
     std::string getName() const { return name; }
     int getStreak() const { return streak; }
     const std::vector<bool>& getHistory() const { return history; }
-
-    virtual std::string toString() const = 0;
-    virtual void print() const = 0;
+    int getTargetDaysPerWeek() const { return targetDaysPerWeek; }
 };
 #endif
