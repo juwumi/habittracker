7b80aa7e47e55e09fb55b509f9ff147e1a853eac
ialek <taisya-alekseenko@mail.ru>
2026-03-09 22:54:02 +0300

    Добавила numericHabit

---
+++ src/numericHabit.cpp
@@ -0,0 +1,25 @@
+#include "numericHabit.h"
+
+numericHabit::numericHabit(const std::string &name, double goal, const std::string &unit, int targetDaysPerWeek)
+    : habit(name, targetDaysPerWeek), goal(goal), unit(unit){}
+
+std::string numericHabit::getType() const {
+    return "numericHabit";
+}
+
+void numericHabit::setValue(double value) {
+    currentValue=value;
+    markCompleted(value>=goal);
+}
+
+std::optional<double> numericHabit::getProgress() const {
+    if (goal==0) return std::nullopt;
+    return (currentValue/goal)*100;
+}
+
+std::string numericHabit::toString() const {
+    std::string status=isGoalReached()?"reached":"not reached";
+    return name+ "| goal: " + std::to_string(goal) + " " + unit + " | now: " + std::to_string(currentValue) + " " + unit + " |  " + status;
+}
+
+
