51df037679df318bce932783663ee3977aa5d28f
Liza <elizaveta.sadykhova@gmail.com>
2026-03-10 20:53:08 +0300

    wrote Statistics.cpp

---
+++ src/Statistics.cpp
@@ -0,0 +1,9 @@
+#include "Statistics.h"
+
+int Statistics::calculateStreak(int habitId, const auto& logs) const {
+    return 0;
+}
+
+double Statistics::calculateCompletionRate(int habitId, int days, const auto& logs) const {
+    return 0.0;
+}
